19e0896bff38e3c8699f4ae7c334542d5e7c07ce
jeremysr <jeremysr@protonmail.com>
2023-09-10 12:15:48 -0600

    schema edits

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,8 @@ USE employees_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL,
+    ON DELETE SET NULL
 );
 
 CREATE TABLE role (
@@ -23,6 +24,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     FOREIGN KEY (role_id)
     REFERENCES role(id),
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-)
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id),
+    ON DELETE SET NULL
+);
